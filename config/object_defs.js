cd38e64dcc61b0ef66d2f41a709b061182d7461d
1egoman <rsg1egoman@gmail.com>
2014-09-05 17:16:13 -0400

    added data key

--- config/object_defs.js
+++ config/object_defs.js
@@ -86,6 +86,15 @@ module.exports = {
       }
     },
 
+    {
+      "value": {
+        "equalTo": "date"
+      },
+      "then": {
+        "means": CONCEPTS.ABSTRACT.DATE
+      }
+    },
+
 
     /**
       Some beverages
